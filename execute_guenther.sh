9ec623b33430fc97b852098959ccc6c222debd18
Matthias <mattwi@fysik.dtu.dk>
2021-07-26 17:52:45 +0200

    Add guenther test file and various interpolations

--- execute_guenther.sh
+++ execute_guenther.sh
@@ -4,6 +4,6 @@
 # If feltor is not here then change the FELTOR_PATH enviromnent variable
 # export FELTOR_PATH="path/to/feltor"
 
-make -C $FELTOR_PATH/inc/geometries/ ds_t
+make -C $FELTOR_PATH/inc/geometries/ ds_guenther_t
 
-cat $1 | jq '.n, .Nx, .Ny, .Nz, .mx, .my, .method' | $FELTOR_PATH/inc/geometries/ds_t > "$2"
+cat $1 | jq '.n, .Nx, .Ny, .Nz, .mx, .my, .method' | $FELTOR_PATH/inc/geometries/ds_guenther_t > "$2"
