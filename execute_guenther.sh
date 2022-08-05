5988d91edc32f639116eef0814ddf51922fb90b9
Matthias <mattwi@fysik.dtu.dk>
2022-08-05 16:02:24 +0200

    Execute and update the ds notebooks

--- execute_guenther.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-: ${FELTOR_PATH:="../feltor"}
-# If feltor is not here then change the FELTOR_PATH enviromnent variable
-# export FELTOR_PATH="path/to/feltor"
-
-make -C $FELTOR_PATH/inc/geometries/ ds_guenther_t
-
-cat $1 | jq '.n, .Nx, .Ny, .Nz, .mx, .my, .method' | $FELTOR_PATH/inc/geometries/ds_guenther_t > "$2"
